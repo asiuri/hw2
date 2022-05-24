22ad8ff7528fe872fe903a5990557b7f0117e71e
asiuri <104019846+asiuri@users.noreply.github.com>
2022-05-24 10:27:43 +0200

    Update extra1.py

--- heandlers1/extra1.py
+++ heandlers1/extra1.py
@@ -12,7 +12,7 @@ async def echo(message: types.Message):
         await bot.pin_chat_message(message.chat.id, message.message_id)
 
     if message.text.lower() == "game":
-        await bot.send_game(message.chat.id, emoji=value)
+        await bot.send_dice(message.chat.id, emoji=value)
 
 def register_hendler_extra(dp: Dispatcher):
     dp.register_message_handler(echo)
