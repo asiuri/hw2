83074ece919f0040e18bf7e16613d853d88508ea
asiuri <104019846+asiuri@users.noreply.github.com>
2022-05-24 11:39:12 +0200

    Update extra1.py

--- heandlers1/extra1.py
+++ heandlers1/extra1.py
@@ -2,8 +2,6 @@ from aiogram import types, Dispatcher
 from config1 import bot, dp
 import random
 
-games = ["🎲","🏐","🏸"]
-value = random.choice(games)
 
 
 # @dp.message_handler()
@@ -12,7 +10,9 @@ async def echo(message: types.Message):
         await bot.pin_chat_message(message.chat.id, message.message_id)
 
     if message.text.lower() == "game":
+        games = random.choice(["🎲", "⚽️", "🏀", "🎯", "🎳", "🎰"])
         await bot.send_dice(message.chat.id, emoji=value)
 
 def register_hendler_extra(dp: Dispatcher):
     dp.register_message_handler(echo)
+  
