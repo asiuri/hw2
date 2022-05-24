7d778b0d83a83c4eeba29fdd339a12371ce16f87
asiuri <104019846+asiuri@users.noreply.github.com>
2022-05-24 10:20:15 +0200

    Update client_kb1.py

--- keyboards1/client_kb1.py
+++ keyboards1/client_kb1.py
@@ -3,12 +3,11 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 b1= KeyboardButton("/start")
 b2= KeyboardButton("/quiz")
 b3= KeyboardButton("/Timetable")
-b4= KeyboardButton("/anekdot")
-b5=KeyboardButton("/mem")
+b4=KeyboardButton("/mem")
 
 
 kb_client= ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True,)
 
-kb_client.add(b1).row(b2,b3,b4).insert(b5)
+kb_client.add(b1).row(b2,b3).insert(b4)
 
 
